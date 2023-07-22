d0413afed0c6c4ed90823e03763d1d1a0e3c21bf
Chris Allen <gh.84kjuqg365@gmail.com>
2023-07-22 13:48:03 -0700

    Design subscription form 1

--- src/app/subscription-form/subscription-form.component.ts
+++ src/app/subscription-form/subscription-form.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-subscription-form',
   templateUrl: './subscription-form.component.html',
   styleUrls: ['./subscription-form.component.css']
 })
-export class SubscriptionFormComponent {
+export class SubscriptionFormComponent implements OnInit {
+  constructor() {}
 
+  ngOnInit(): void {
+    ;
+  }
 }
